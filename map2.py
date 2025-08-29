99e1b29a4dc07c9c7ef14d1701cd6e24e60f6929
Max Mayekar <suyashmayekar13@gmail.com>
2025-08-29 13:34:52 +0530

    Update map2.py

--- map2.py
+++ map2.py
@@ -2,3 +2,19 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 G = nx.Graph()
+
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
+
+
+G.add_edge("A", "B")
+G.add_edge("B", "C")
+
+nx.write_graphml(G, "my_graph.graphml")
+
+print("GraphML file created: my_graph.graphml")
+
+nx.draw(G, with_labels=True, node_size=800, node_color="lightblue", font_size=10)
+plt.show()
+
